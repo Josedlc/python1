97bd3d2c1aa41323b5a2303baefb06acf8a8c769
Josedlc <josedlc600@gmail.com>
2021-11-02 15:04:32 -0700

    file

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,25 @@
-print 'hello world'
+var = 'mundo'
+num = 1234
+num2 = 5678
+
+flo = 123.4
+flo2 = 456.3
+carac = 'c'
+carac2 = 'd'
+
+bol = True
+bol2 = False
+bol3 = None
+
+print [var]
+
+print [carac + carac2]
+
+print [flo + flo2]
+
+print [bol + bol2 + bol3]
+
+for x in range(1, 100, 2):
+  print(x)
+
+
